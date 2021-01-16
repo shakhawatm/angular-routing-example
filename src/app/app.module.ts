15cbad3040476e72cea288d41ee6f7f13c0a1fae
Md. Shakhawat Hossain Mollah <sh.sumon99@gmail.com>
2021-01-16 20:19:55 +0600

    All pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ContactComponent } from './contact/contact.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ProfileComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([ 
+      { path: '', redirectTo:"/home", pathMatch:"full" },
+      { path: 'home', component: HomeComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'profile', component: ProfileComponent },
+      { path: 'contact', component: ContactComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
